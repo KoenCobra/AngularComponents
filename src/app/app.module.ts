9ac04d115797604f460af5334aabb6c2af2438a5
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-20 17:46:18 +0200

    Added lazy loading

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,6 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import {ElementsModule} from "./elements/elements.module";
 import {CollectionsModule} from "./collections/collections.module";
 import {ModsModule} from "./mods/mods.module";
 import {ViewsModule} from "./views/views.module";
@@ -18,7 +17,6 @@ import { NotFoundComponent } from './not-found/not-found.component';
   ],
   imports: [
     BrowserModule,
-    ElementsModule,
     CollectionsModule,
     ModsModule,
     ViewsModule,
