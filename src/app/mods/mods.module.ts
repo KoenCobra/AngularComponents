5c9d02439227a4f95c7cc46df35a52ff076a8c97
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-20 17:05:57 +0200

    added home components

--- src/app/mods/mods.module.ts
+++ src/app/mods/mods.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ModsRoutingModule } from './mods-routing.module';
+import { ModsHomeComponent } from './mods-home/mods-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ModsHomeComponent
+  ],
+  exports: [
+    ModsHomeComponent
+  ],
   imports: [
     CommonModule,
     ModsRoutingModule
--- src/app/views/views.module.ts
+++ src/app/views/views.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ViewsRoutingModule } from './views-routing.module';
+import { ViewsHomeComponent } from './views-home/views-home.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ViewsHomeComponent
+  ],
+  exports: [
+    ViewsHomeComponent
+  ],
   imports: [
     CommonModule,
     ViewsRoutingModule
