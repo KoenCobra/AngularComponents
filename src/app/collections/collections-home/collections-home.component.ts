aa1ab5278d5a5d1f6f493bc5c753cda85b83f0a8
Koen De Groot <koen.de.groot@organimmo.be>
2023-04-21 11:00:25 +0200

    Added [routerLinkActiveOptions]="{exact:true}

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -6,7 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./collections-home.component.scss']
 })
 export class CollectionsHomeComponent {
-  partOfLink = 'hi';
   data = [
     { name: 'James', age: 24, job: 'Designer', employed:true },
     { name: 'Jill', age: 26, job: 'Engineer', employed:true },
